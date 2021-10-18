744065d92c02e7ce0b2f7965618f5ba48d7cc4dd
Opilane TTHK <opilane@tthk.ee>
2021-10-18 12:28:52 +0300

    FizzBuzz Tasks

--- src/ee/tthk/tarpv19/basharov/FizzBuzz.java
+++ src/ee/tthk/tarpv19/basharov/FizzBuzz.java
@@ -2,9 +2,18 @@ package ee.tthk.tarpv19.basharov;
 
 public class FizzBuzz {
 
-	public int[] getNumbers() {
-		// TODO Auto-generated method stub
-		return new int[100];
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++){
+			System.out.println(fiz(i));
+		}
+	}
+
+	public static String fiz(int i) {
+		if(i == 0) return "0";
+		if(i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
+		if(i % 3 == 0) return "Fizz";
+		if(i % 5 == 0) return "Buzz";
+		return String.valueOf(i);
 	}
 
 }
--- src/ee/tthk/tarpv19/basharov/FizzBuzzTests.java
+++ src/ee/tthk/tarpv19/basharov/FizzBuzzTests.java
@@ -16,10 +16,45 @@ class FizzBuzzTests {
 	void tearDown() throws Exception {
 	}
 
-	@Test
+	/*@Test
 	void shouldReturnOneHundredNumbers() {
 		FizzBuzz fb=new FizzBuzz();
 		assertEquals(100, fb.getNumbers().length);
+	}*/
+	
+	@Test
+	public void check_0is0() {
+		assertEquals(FizzBuzz.fiz(0), ("0"));
+	}
+	
+	@Test
+	public void check_1is1() {
+		assertEquals(FizzBuzz.fiz(1), ("1"));
+	}
+	
+	@Test
+	public void check_3isFizz() {
+		assertEquals(FizzBuzz.fiz(3), ("Fizz"));
+	}
+	
+	@Test
+	public void check_5isBuzz() {
+		assertEquals(FizzBuzz.fiz(5), ("Buzz"));
+	}
+	
+	@Test
+	public void check_multipleOf3isFizz() {
+		assertEquals(FizzBuzz.fiz(6), ("Fizz"));
+	}
+	
+	@Test
+	public void check_multipleOf5isBuzz() {
+		assertEquals(FizzBuzz.fiz(10), ("Buzz"));
+	}
+	
+	@Test
+	public void check_multipleOf5And3IsBuzz() {
+		assertEquals(FizzBuzz.fiz(15), ("FizzBuzz"));
 	}
 
 }
