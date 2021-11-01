3486f759e1dae065060c4e73f45a92059b26b276
Opilane TTHK <opilane@tthk.ee>
2021-11-01 11:24:33 +0200

    Test #8 passed

--- src/ee/tthk/tarpv19/basharov/IpValidatorTests.java
+++ src/ee/tthk/tarpv19/basharov/IpValidatorTests.java
@@ -58,7 +58,7 @@ class IpValidatorTests {
 	@Test
 	void ShouldBe_False_GivenIPstartingWith255() {
 		IpValidator validator= new IpValidator();
-		assertTrue(validator.ValidateIpv4Address("255.2.3.4"));
+		assertFalse(validator.ValidateIpv4Address("255.2.3.4"));
 	}
 	@Test
 	void ShouldBe_False_GivenIPendingWith255() {
